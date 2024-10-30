a04f1cd9ff50da2c2b6f43ffb1ea46fa797c4f6b
syedabdullahzaidi786 <ardevelopers622@gmail.com>
2024-10-30 23:59:18 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+}
+
+module.exports = nextConfig
